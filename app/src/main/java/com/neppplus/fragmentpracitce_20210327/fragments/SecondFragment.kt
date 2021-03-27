8d6e4e3e51b8380b1b1ed1fec6d9b26a5c133377
전은형 <ooooo5h91@gmail.com>
2021-03-27 12:35:38 +0900

    두번째 프래그먼트 추가작업중

---
+++ app/src/main/java/com/neppplus/fragmentpracitce_20210327/fragments/SecondFragment.kt
@@ -0,0 +1,28 @@
+package com.neppplus.fragmentpracitce_20210327.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.neppplus.fragmentpracitce_20210327.R
+import kotlinx.android.synthetic.main.fragment_second.*
+
+class SecondFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        }
+
+    }
+}
